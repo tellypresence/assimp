957e8009728442854606aeb6f6c9b9e77cda4b45
Steve M <praktique-tellypresence@yahoo.com>
2024-04-24 22:08:22 -0700

    Add (untested) tinyusdz to assimp 4x4 matrix converter

--- code/AssetLib/USD/USDLoaderImplTinyusdz.cpp
+++ code/AssetLib/USD/USDLoaderImplTinyusdz.cpp
@@ -236,6 +236,27 @@ aiNode *USDImporterImplTinyusdz::nodes(
     return nodesRecursive(nullptr, render_scene.nodes[0]);
 }
 
+static aiMatrix4x4 tinyUsdzMat4ToAiMat4(const double matIn[4][4]) {
+    aiMatrix4x4 matOut;
+    matOut.a1 = matIn[0][0];
+    matOut.a2 = matIn[0][1];
+    matOut.a3 = matIn[0][2];
+    matOut.a4 = matIn[0][3];
+    matOut.b1 = matIn[1][0];
+    matOut.b2 = matIn[1][1];
+    matOut.b3 = matIn[1][2];
+    matOut.b4 = matIn[1][3];
+    matOut.c1 = matIn[2][0];
+    matOut.c2 = matIn[2][1];
+    matOut.c3 = matIn[2][2];
+    matOut.c4 = matIn[2][3];
+    matOut.d1 = matIn[3][0];
+    matOut.d2 = matIn[3][1];
+    matOut.d3 = matIn[3][2];
+    matOut.d4 = matIn[3][3];
+    return matOut;
+}
+
 aiNode *USDImporterImplTinyusdz::nodesRecursive(
         aiNode *pNodeParent,
         const tinyusdz::tydra::Node &node) {
@@ -243,6 +264,7 @@ aiNode *USDImporterImplTinyusdz::nodesRecursive(
     aiNode *cNode = new aiNode();
     cNode->mParent = pNodeParent;
     cNode->mName.Set(node.prim_name);
+    cNode->mTransformation = tinyUsdzMat4ToAiMat4(node.local_matrix.m);
 //    ss.str("");
 //    ss << "nodesRecursive(): node " << cNode->mName.C_Str() <<
 //            " disp " << node.display_name << ", abs " << node.abs_path;
